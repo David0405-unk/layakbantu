738ae1ab6a992dda3cd781580451bac9c4d8942a
David0405-unk <s22310234@student.unklab.ac.id>
2026-09-08 17:37:20 +0700

    update tampilan mobile

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import supabase from '../lib/supabaseClient';
+import {supabase} from '../lib/supabaseClient';
 
 function ForgotPassword() {
   const [email, setEmail] = useState('');
