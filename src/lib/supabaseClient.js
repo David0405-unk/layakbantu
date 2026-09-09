aafcb86f85866234c521749f45fe2aaa0127de8f
David0405-unk <s22310234@student.unklab.ac.id>
2026-09-09 07:46:21 +0700

    fix netlify

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,3 +4,5 @@ const supabaseUrl = 'https://jyjsieudhmsfazfbsger.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5anNpZXVkaG1zZmF6ZmJzZ2VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MTM5MjYsImV4cCI6MjEwNDA4OTkyNn0.hxMVAluxym6c7hdG3UjjtC0hedQeY4qstBwLI9U0Pfo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
